a418ce4c96bcbf8b24c9f28f1d001fc97a6bd51f
Zachary Coursey <zcoursey22@gmail.com>
2018-02-03 23:17:00 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -6,7 +6,7 @@ function init() {
   document.querySelector('body').addEventListener('keydown', function(e) {
     if (parseInt(e.which) === code[index]) {
       if (index === code.length - 1) {
-        alert("Konami!");
+        window.alert("Konami!");
         index = 0;
       } else index++;
     } else {
